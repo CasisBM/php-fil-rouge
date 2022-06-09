e7988e14a2aea6e092aebc571dba7673aebe973e
CasisBM <casis.bouissamatoko@gmail.com>
2022-06-09 16:55:43 +0200

    ajout class sql

--- classes/Sql.php
+++ classes/Sql.php
@@ -1,12 +1,32 @@
 <?php class Sql
 {
-    private $serverName;
-    private $userName;
-    private $userPassword;
-    private $database; 
+    private $serverName = "localhost";
+    private $userName = "root";
+    private $userPassword = "";
+    private $database = "filrouge";
+    private $connexion;
     
     public function __construct(string $serverName,$userName,$userPassword,$database)
     {
-        
+        try {
+            //code...
+            $this->connexion = new PDO("mysql:host=$serverName;dbname=$database", $userName, $userPassword);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die("Erreur : ".$e->getMessage() );
+        }  
     }
+
+    public function inserer($requete)
+    {
+        $this->connexion->exec($requete);
+    }
+
+    public function __destruct()
+    {
+        $this->connexion = null;
+    }
+
+    
 }
